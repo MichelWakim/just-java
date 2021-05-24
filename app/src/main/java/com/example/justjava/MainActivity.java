34c2726c52a684b582c13832bc3a9bd3c02ab1c6
Michel <micho.courses@gmail.com>
2021-05-24 17:40:57 +0200

    fixing the error

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.justjava;
 
-import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
